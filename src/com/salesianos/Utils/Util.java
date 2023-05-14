79e08a68012bd115e413cc6f33f4b5311e111b34
Manuel <mgamez244@gmail.com>
2023-05-14 20:54:53 +0100

    feat(Util) Creada la clase util con su paquete correspondiente

---
+++ src/com/salesianos/Utils/Util.java
@@ -0,0 +1,23 @@
+package com.salesianos.Utils;
+
+public class Util {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_MAGENTA = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_GRAY = "\u001B[37m";
+    public static final String ANSI_WHITE = "\u001B[38m";
+
+    public static final String BACKGROUND_WHITE = "\u001B[39m";
+    public static final String BACKGROUND_BLACK = "\u001B[40m";
+    public static final String BACKGROUND_RED = "\u001B[41m";
+    public static final String BACKGROUND_GREEN = "\u001B[42m";
+    public static final String BACKGROUND_YELLOW = "\u001B[43m";
+    public static final String BACKGROUND_BLUE = "\u001B[44m";
+    public static final String BACKGROUND_MAGENT = "\u001B[45m";
+    public static final String BACKGROUND_CYAN = "\u001B[46m";
+    public static final String BACKGROUND_GRAY = "\u001B[47m";
+}
