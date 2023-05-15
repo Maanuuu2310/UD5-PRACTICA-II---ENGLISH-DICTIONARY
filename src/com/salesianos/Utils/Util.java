6a764c82c3b15e5afb626348f37df804753c1b4d
Manuel <mgamez244@gmail.com>
2023-05-15 10:34:41 +0100

    fix(Util) he actualizado el codigo de la clase utils eliminando ciertos apartados

--- src/com/salesianos/Utils/Util.java
+++ src/com/salesianos/Utils/Util.java
@@ -11,14 +11,4 @@ public class Util {
     public static final String ANSI_GRAY = "\u001B[37m";
     public static final String ANSI_WHITE = "\u001B[38m";
     public static final String ANSI_RESET = "\u001B[0m";
-
-    public static final String BACKGROUND_WHITE = "\u001B[39m";
-    public static final String BACKGROUND_BLACK = "\u001B[40m";
-    public static final String BACKGROUND_RED = "\u001B[41m";
-    public static final String BACKGROUND_GREEN = "\u001B[42m";
-    public static final String BACKGROUND_YELLOW = "\u001B[43m";
-    public static final String BACKGROUND_BLUE = "\u001B[44m";
-    public static final String BACKGROUND_MAGENT = "\u001B[45m";
-    public static final String BACKGROUND_CYAN = "\u001B[46m";
-    public static final String BACKGROUND_GRAY = "\u001B[47m";
 }
